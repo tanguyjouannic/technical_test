914056170067e97d958c7e9e4f50adee23eddafe
Tanguy Jouannic <tanguy.jouannic@gmail.com>
2024-12-06 11:34:38 +0100

    #0 Add a ui sample test

--- clocks-ui/src/App.test.js
+++ clocks-ui/src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Clock from './Clock';
+import moment from 'moment-timezone';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders clock with correct label and time', () => {
+  const timezone = 'Europe/London';
+  const label = 'London Clock';
+  render(<Clock label={label} timezone={timezone} />);
+  expect(screen.getByText(label)).toBeInTheDocument();
+  const currentTime = moment.tz(timezone).format('HH:mm:ss');
+  expect(screen.getByText(currentTime)).toBeInTheDocument();
 });
